e5260decced00afb93391d58ce161362bf22f5d1
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2020-01-07 16:54:04 +0000

    book class has title

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,3 +1,22 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BookTest {
+
+    private Book book1;
+
+    @Before
+    public void before() {
+        book1 = new Book("The Hobbit", "JRR Tolkien", "Fantasy");
+    }
+
+    @Test
+    public void hasTitle() {
+        assertEquals("The Hobbit", book1.getTitle());
+    }
+
+
 
 }
